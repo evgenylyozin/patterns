4b69656da497b941b9a779c919bd4b5383b43843
Evgeny Lyozin <evgenylyozindev@gmail.com>
2023-02-09 17:36:28 +0300

    Добавлен паттерн Декоратор (Decorator)

--- go/oop-patterns.go
+++ go/oop-patterns.go
@@ -401,8 +401,6 @@ func (d *director) buildHouse() house {
 // To organize relationships between objects
 
 // --todo--
-// - Composite
-// - Decorator
 // - Facade
 // - Flyweight
 // - Proxy
@@ -642,6 +640,75 @@ func recursivelyPrintFoldersData(folder folder) {
 // folder5.addFile(file10)
 
 // recursivelyPrintFoldersData(*folder1)
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Decorator (ДЕКОРАТОР)
+// ЦЕЛЬ: расширить функционал конкретного объекта, не меняя при этом логики
+// класса (схемы), из которого был создан объект
+
+// Интерфейс объекта, который оборачивается
+// ему должны так же следовать все обёртки, чтобы клиентский код
+// мог без труда использовать столько декораторов, сколько угодно
+type iDecorated interface {
+	log(data string)
+}
+
+// Конкретный объект со стандартной имплементацией операций
+type decorated struct {
+}
+
+func (d *decorated) log(data string) {
+	fmt.Println(data)
+}
+
+// Базовый декоратор должен так же следовать интерфейсу оборачиваемого объекта
+type decorator struct {
+	decorated iDecorated
+}
+
+func (d *decorator) log(data string) {
+	d.decorated.log(data)
+}
+
+// Конкретные декораторы следуют интерфейсу оборачиваемого объекта и
+// манипулируют данными, передаваемыми в него или получаемыми от него
+type concreteDecoratorA struct {
+	decorator
+}
+
+func (d *concreteDecoratorA) log(data string) {
+	data = fmt.Sprintf("Декоратор А преобразовал данные, строка до преобразования:\n%v", data)
+	d.decorator.log(data)
+}
+
+func newConcreteDecoratorA(d iDecorated) *concreteDecoratorA {
+	return &concreteDecoratorA{decorator{decorated: d}}
+}
+
+type concreteDecoratorB struct {
+	decorator
+}
+
+func (d *concreteDecoratorB) log(data string) {
+	data = fmt.Sprintf("Декоратор B преобразовал данные, строка до преобразования:\n%v", data)
+	d.decorator.log(data)
+}
+
+func newConcreteDecoratorB(d iDecorated) *concreteDecoratorB {
+	return &concreteDecoratorB{decorator{decorated: d}}
+}
+
+// decorated := &decorated{}
+// decoratorA := newConcreteDecoratorA(decorated)
+// decoratorB := newConcreteDecoratorB(decoratorA)
+
+// decorated.log("Привет!")
+// decoratorA.log("Привет!")
+// decoratorB.log("Привет!")
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
--- typescript/oop-patterns.ts
+++ typescript/oop-patterns.ts
@@ -595,6 +595,62 @@ class Folder {
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
+// Decorator (ДЕКОРАТОР)
+// ЦЕЛЬ: расширить функционал конкретного объекта, не меняя при этом логики
+// класса (схемы), из которого был создан объект
+
+// Интерфейс объекта, который оборачивается
+// ему должны так же следовать все обёртки, чтобы клиентский код
+// мог без труда использовать столько декораторов, сколько угодно
+interface IDecorated {
+  log(data: string): void;
+}
+
+// Конкретный класс со стандартной имплементацией операций
+class Decorated implements IDecorated {
+  public log(data: string): void {
+    console.log(data);
+  }
+}
+
+// Базовый декоратор должен так же следовать интерфейсу оборачиваемого объекта
+class Decorator implements IDecorated {
+  protected decorated: IDecorated;
+
+  constructor(decorated: IDecorated) {
+    this.decorated = decorated;
+  }
+  public log(data: string): void {
+    this.decorated.log(data);
+  }
+}
+
+// Конкретные декораторы следуют интерфейсу оборачиваемого объекта и
+// манипулируют данными, передаваемыми в него или получаемыми от него
+class ConcreteDecoratorA extends Decorator {
+  public log(data: string): void {
+    data = `Декоратор А преобразовал данные, строка до преобразования:\n${data}`;
+    super.log(data);
+  }
+}
+
+class ConcreteDecoratorB extends Decorator {
+  public log(data: string): void {
+    data = `Декоратор B преобразовал данные, строка до преобразования:\n${data}`;
+    super.log(data);
+  }
+}
+
+// const decorated = new Decorated();
+// const decoratorA = new ConcreteDecoratorA(decorated);
+// const decoratorB = new ConcreteDecoratorB(decoratorA);
+
+// decorated.log('Привет!');
+// decoratorA.log('Привет!');
+// decoratorB.log('Привет!');
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // BEHAVIORAL DESIGN PATTERNS
 // To handle communication between heterogeneous objects
 
