5d9b1dd1bfd8e222ddb46834d94f79394b7fdc3a
Evgeny Lyozin <evgenylyozindev@gmail.com>
2023-02-11 09:44:16 +0300

    Добавлен паттерн Observer

--- go/oop-patterns.go
+++ go/oop-patterns.go
@@ -1388,4 +1388,113 @@ func (c *caretaker) showHistory() {
 
 // fmt.Println("\nClient: Once more!")
 // caretaker.undo()
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// OBSERVER (НАБЛЮДАТЕЛЬ)
+// ЦЕЛЬ: организовать систему оповещений о событиях, происходящих с каким-либо
+// объектом
+
+// Интерфейс наблюдателя содержит методы, которые дают каблюдателю
+// возможность получать уведомления от субъекта
+type observer interface {
+	notify(evt string)
+	getName() string
+}
+
+// Интерфейс Publisher определяет возможность подписываться и отписываться от
+// уведомлений от субъекта, а так же имеет метод для оповещения всех
+// текущих подписчиков субъекта
+type publisher interface {
+	subscribe(obs observer) publisher
+	unsubscribe(obs observer) publisher
+	notifyAll(evt string)
+}
+
+type concreteObserver1 struct {
+}
+
+func (o *concreteObserver1) notify(evt string) {
+	fmt.Println("Первый конкретный наблюдатель получил уведомление:", evt)
+}
+func (o *concreteObserver1) getName() string {
+	return "ConcreteObserver1"
+}
+
+type concreteObserver2 struct {
+}
+
+func (o *concreteObserver2) notify(evt string) {
+	fmt.Println("Второй конкретный наблюдатель получил уведомление:", evt)
+}
+func (o *concreteObserver2) getName() string {
+	return "ConcreteObserver2"
+}
+
+type concreteObserver3 struct {
+}
+
+func (o *concreteObserver3) notify(evt string) {
+	fmt.Println("Третий конкретный наблюдатель получил уведомление:", evt)
+}
+func (o *concreteObserver3) getName() string {
+	return "ConcreteObserver3"
+}
+
+type concretePublisher struct {
+	observers []observer
+}
+
+func (p *concretePublisher) notifyAll(evt string) {
+	for _, observer := range p.observers {
+		(observer).notify(evt)
+	}
+}
+func (p *concretePublisher) subscribe(obs observer) publisher {
+	p.observers = append(p.observers, obs)
+	return p
+}
+
+func (p *concretePublisher) unsubscribe(obs observer) publisher {
+	idx := -1
+	for i, observer := range p.observers {
+
+		if observer == obs {
+			idx = i
+			break
+		}
+	}
+	if idx != -1 {
+		rest := make([]observer, 0)
+		if (idx + 1) < len(p.observers) {
+			rest = append(rest, p.observers[idx+1:]...)
+		}
+		p.observers = append(p.observers[0:idx], rest...)
+	}
+	return p
+}
+func (p *concretePublisher) printObservers() {
+	for _, obs := range p.observers {
+		fmt.Println((obs).getName())
+	}
+}
+
+// publisher := concretePublisher{}
+
+// observer1 := &concreteObserver1{}
+// observer2 := &concreteObserver2{}
+// observer3 := &concreteObserver3{}
+
+// publisher.subscribe(observer1).subscribe(observer2).subscribe(observer3)
+
+// publisher.printObservers()
+
+// publisher.notifyAll("ПРИВЕТ!")
+
+// publisher.unsubscribe(observer2)
+
+// publisher.printObservers()
+
+// publisher.notifyAll("ПОКА!")
+
 //------------------------------------------------------------------------------
--- typescript/oop-patterns.ts
+++ typescript/oop-patterns.ts
@@ -1380,3 +1380,97 @@ class Caretaker {
 // caretaker.undo();
 
 //------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// OBSERVER (НАБЛЮДАТЕЛЬ)
+// ЦЕЛЬ: организовать систему оповещений о событиях, происходящих с каким-либо
+// объектом
+
+// Интерфейс наблюдателя содержит методы, которые дают каблюдателю
+// возможность получать уведомления от субъекта
+interface Observer {
+  notify(evt: string): void;
+  getName(): string;
+}
+// Интерфейс Publisher определяет возможность подписываться и отписываться от
+// уведомлений от субъекта, а так же имеет метод для оповещения всех
+// текущих подписчиков субъекта
+interface Publisher {
+  subscribe(obs: Observer): Publisher;
+  unsubscribe(obs: Observer): Publisher;
+  notifyAll(evt: string): void;
+}
+
+class ConcreteObserver1 implements Observer {
+  public notify(evt: string): void {
+    console.log('Первый конкретный наблюдатель получил уведомление:', evt);
+  }
+  public getName(): string {
+    return 'ConcreteObserver1';
+  }
+}
+
+class ConcreteObserver2 implements Observer {
+  public notify(evt: string): void {
+    console.log('Второй конкретный наблюдатель получил уведомление:', evt);
+  }
+  public getName(): string {
+    return 'ConcreteObserver2';
+  }
+}
+
+class ConcreteObserver3 implements Observer {
+  public notify(evt: string): void {
+    console.log('Третий конкретный наблюдатель получил уведомление:', evt);
+  }
+  public getName(): string {
+    return 'ConcreteObserver3';
+  }
+}
+
+class ConcretePublisher implements Publisher {
+  private observers: Observer[] = [];
+  public notifyAll(evt: string): void {
+    for (let observer of this.observers) {
+      observer.notify(evt);
+    }
+  }
+  public subscribe(obs: Observer): Publisher {
+    this.observers.push(obs);
+    return this;
+  }
+
+  public unsubscribe(obs: Observer): Publisher {
+    const idx = this.observers.findIndex((observ) => observ === obs);
+    if (idx !== -1) {
+      this.observers = [
+        ...this.observers.slice(0, idx),
+        ...this.observers.slice(idx + 1),
+      ];
+    }
+    return this;
+  }
+  public printObservers(): void {
+    for (let obs of this.observers) {
+      console.log(obs.getName());
+    }
+  }
+}
+
+// const publisher = new ConcretePublisher();
+
+// const observer1 = new ConcreteObserver1();
+// const observer2 = new ConcreteObserver2();
+// const observer3 = new ConcreteObserver3();
+
+// publisher.subscribe(observer1).subscribe(observer2).subscribe(observer3);
+
+// publisher.printObservers();
+
+// publisher.notifyAll('ПРИВЕТ!');
+
+// publisher.unsubscribe(observer2);
+
+// publisher.printObservers();
+
+// publisher.notifyAll('ПОКА!');
+//------------------------------------------------------------------------------
