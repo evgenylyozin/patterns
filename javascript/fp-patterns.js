e2e0ff566079db313c61c95cadc7a67c17c54ac6
Evgeny Lyozin <evgenylyozindev@gmail.com>
2023-02-13 15:34:45 +0300

    Добавлен паттерн каррирование

--- javascript/fp-patterns.js
+++ javascript/fp-patterns.js
@@ -162,3 +162,33 @@ const dude2 = {
 
 // printThisDude(dude1);
 // printThisDude(dude2);
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Curring (Каррирование)
+// Цель: зафиксировать повторяющиеся аргументы при вызове функции, получив
+// новую функцию
+
+const curry = (func) => {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
+  };
+};
+
+const addThreeNums = (a, b, c) => {
+  return a + b + c;
+};
+
+const curriedAddThreeNums = curry(addThreeNums);
+
+// console.log(curriedAddThreeNums(1, 2, 3));
+// console.log(curriedAddThreeNums(1)(2, 3));
+// console.log(curriedAddThreeNums(1, 2)(3));
+// console.log(curriedAddThreeNums(1)(2)(3));
+// console.log(curriedAddThreeNums(1, 2, 3, 4, 5, 6, 7, 8));
--- typescript/fp-patterns.ts
+++ typescript/fp-patterns.ts
@@ -173,3 +173,33 @@ const dude2: config = {
 
 // printThisDude(dude1);
 // printThisDude(dude2);
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Curring (Каррирование)
+// Цель: зафиксировать повторяющиеся аргументы при вызове функции, получив
+// новую функцию
+
+const curry = (func: (...args: any) => any) => {
+  return function curried(...args: any) {
+    if (args.length >= func.length) {
+      return func.apply(null, args);
+    } else {
+      return function (...args2: any) {
+        return curried.apply(null, args.concat(args2));
+      };
+    }
+  };
+};
+
+const addThreeNums = (a: number, b: number, c: number) => {
+  return a + b + c;
+};
+
+const curriedAddThreeNums = curry(addThreeNums);
+
+// console.log(curriedAddThreeNums(1, 2, 3));
+// console.log(curriedAddThreeNums(1)(2, 3));
+// console.log(curriedAddThreeNums(1, 2)(3));
+// console.log(curriedAddThreeNums(1)(2)(3));
+// console.log(curriedAddThreeNums(1, 2, 3, 4, 5, 6, 7, 8));
