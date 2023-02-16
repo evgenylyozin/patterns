47d1a4021fb419212aa16c30cc753d637e3c88bf
Evgeny Lyozin <evgenylyozindev@gmail.com>
2023-02-16 14:20:31 +0300

    документация каррирования

--- typescript/fp-patterns/curring/curring.ts
+++ typescript/fp-patterns/curring/curring.ts
@@ -1,9 +1,8 @@
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-// Curring (Каррирование)
-// Цель: зафиксировать повторяющиеся аргументы при вызове функции, получив
-// новую функцию
-
+// Обобщенная функция высшего порядка, вернёт функцию,
+// которая будет ожидать аргументы и будет запоминать их
+// и возвращать очередную функцию, ожидающую аргументы до
+// тех пор, пока их не наберётся достаточное количество
+// (для первоначальной выполнения каррированной функции)
 const curry = (func: (...args: any) => any) => {
   return function curried(...args: any) {
     if (args.length >= func.length) {
@@ -20,10 +19,17 @@ const addThreeNums = (a: number, b: number, c: number) => {
   return a + b + c;
 };
 
+// каррируем функцию addThreeNums,
+// получим функцию, которой можно передать от 0
+// аргументов
 const curriedAddThreeNums = curry(addThreeNums);
 
-// console.log(curriedAddThreeNums(1, 2, 3));
-// console.log(curriedAddThreeNums(1)(2, 3));
-// console.log(curriedAddThreeNums(1, 2)(3));
-// console.log(curriedAddThreeNums(1)(2)(3));
-// console.log(curriedAddThreeNums(1, 2, 3, 4, 5, 6, 7, 8));
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//------------------------------- Клиентский код -------------------------------
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+console.log(curriedAddThreeNums(1, 2, 3));
+console.log(curriedAddThreeNums()()()(1)(2, 3));
+console.log(curriedAddThreeNums(1, 2)(3));
+console.log(curriedAddThreeNums(1)(2)(3));
+console.log(curriedAddThreeNums(1, 2, 3, 4, 5, 6, 7, 8));
