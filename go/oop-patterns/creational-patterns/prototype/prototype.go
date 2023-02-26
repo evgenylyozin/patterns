8bd8ae9217a3034fa1139889e6a5bf14b3ee9131
Evgeny Lyozin <evgenylyozindev@gmail.com>
2023-02-26 11:03:22 +0300

    перевод прототипа

--- go/oop-patterns/creational-patterns/prototype/prototype.go
+++ go/oop-patterns/creational-patterns/prototype/prototype.go
@@ -28,7 +28,7 @@ type concretePrototype2 struct {
 }
 
 func (p *concretePrototype2) print() {
-	fmt.Println(p.name, "my age is", p.age)
+	fmt.Println(p.name, "мой возраст", p.age)
 }
 func (p *concretePrototype2) clone() prototype {
 	return &concretePrototype2{name: p.name, age: p.age}
@@ -39,8 +39,8 @@ func (p *concretePrototype2) clone() prototype {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 func main() {
-	cp1 := concretePrototype1{name: "I'm prototype 1"}
-	cp2 := concretePrototype2{name: "I'm prototype 2", age: 30}
+	cp1 := concretePrototype1{name: "Я прототип 1"}
+	cp2 := concretePrototype2{name: "Я прототип 2", age: 30}
 
 	cloneOfProto1 := cp1.clone()
 	cloneOfProto2 := cp2.clone()
@@ -48,8 +48,11 @@ func main() {
 	cloneOfProto1.print()
 	cloneOfProto2.print()
 
-	cloneOfProto1.(*concretePrototype1).name = "I'm a clone of prototype 1"
-	cloneOfProto2.(*concretePrototype2).name = "I'm a clone of prototype 2"
+	cloneOfProto1.(*concretePrototype1).name = "Я клон прототипа 1"
+	cloneOfProto2.(*concretePrototype2).name = "Я клон прототипа 2"
+	cloneOfProto1.print()
+	cloneOfProto2.print()
+
 	cloneOfProto2.(*concretePrototype2).age = 21
 
 	cloneOfProto1.print()
