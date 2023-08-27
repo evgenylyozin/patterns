c4cea0c7d8ead0b3e2a0c6788d7676b85e8b6556
Евгений Лёзин <evgenylyozindev@gmail.com>
2023-08-27 19:28:35 +0300

    add init scripts

---
+++ run.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+case $1 in
+  "init-project")
+    chmod +x pre-commit && cp pre-commit .git/hooks/ && cd typescript && npm i
+  ;;
+  "copy-docs")
+    cd ./docs/copy-docs/ && ./copy
+  ;;
+  "commit-docs")
+    git add -a && git commit -m "update docs" && git push
+  ;;
+  *)
+    echo "Unknown command $1"
+  ;;
+esac
