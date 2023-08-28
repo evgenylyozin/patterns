7ebef2efc48afd8fde4a4a900ab3e6de96c19a38
Евгений Лёзин <evgenylyozindev@gmail.com>
2023-08-28 07:10:02 +0300

    remove pre-push hook

--- run.sh
+++ run.sh
@@ -2,13 +2,10 @@
 
 case $1 in
   "init-project")
-    chmod +x pre-commit && chmod +x pre-push && cp -t .git/hooks/ pre-commit pre-push && cd typescript && npm i
+    chmod +x pre-commit && cp -t .git/hooks/ pre-commit && cd typescript && npm i
   ;;
   "copy-docs")
     cd ./docs/copy-docs/ && ./copy
-  ;;
-  "commit-docs")
-    git add -A && git commit -m 'Docs update' && git push
   ;;
   *)
     echo "Unknown command $1"
